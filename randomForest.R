c8fd8b4a078397ca114511df6d3912fa1284dd6a
Chris Comiskey <cwcomiskey@gmail.com>
2018-11-03 12:43:49 -0700

    Model fit, started presentation

--- randomForest.R
+++ randomForest.R
@@ -1,4 +1,5 @@
 dat <- cbind.data.frame(Res, Covs3045) 
+
 names(dat) <- make.names(names(dat))
 
 samp <- sample(1:155, 140)
@@ -11,17 +12,20 @@ test <- dat[-samp,]
   Ytest <- test %>%
     select(class) 
 
-rf <- randomForest(class ~ ., data = train, importance = TRUE)  
+rf <- randomForest(class ~ ., data = train, importance = TRUE,
+                   mtry = 100, ntree = 500)  
 
 rf_imp <- rf$importance
-rownames(rf_imp)
   rf_imp_MDA <- rf_imp %>%
     as.data.frame() %>%
     mutate(name = rownames(rf_imp)) %>%
-    arrange(desc(MeanDecreaseAccuracy))
+    arrange(desc(MeanDecreaseAccuracy)) %>%
+    .[1:5,c(4,6)]
   rf_imp_MDG <- rf_imp %>%
     as.data.frame() %>%
-    arrange(desc(MeanDecreaseGini))
+    mutate(name = rownames(rf_imp)) %>%
+    arrange(desc(MeanDecreaseGini)) %>%
+    .[1:5, c(5,  6)]
   
 # Plot  
 randomForest::varImpPlot(rf, type=1, pch=19, col=1, cex=.5, main="")
@@ -30,4 +34,14 @@ randomForest::varImpPlot(rf, type=2, pch=19, col=1, cex=.5, main="")
 mean(predict(rf, newdata = test) == Ytest$class) # Success prob!
 
 ?replicate
-mean(replicate(100, mean(rnorm(25))))
+# mean(replicate(100, mean(rnorm(25))))
+
+# Key question: are variable important measure persistent (correlated) across random samples of train/test?
+for(i in 1:nsim){
+  # Random train/test sample
+  # Fit model
+  # Var Imp rank
+}
+  # Cor()
+  # i.e. Do relative ranks tend to remain similar?
+  # i.e. Do more important variables tend to remain more important across samples, less important remain less?
